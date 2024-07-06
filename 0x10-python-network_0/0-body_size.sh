fe9b5f1f28662d218d97604fd8f74f8ba9f0aa97
Abdulhakim Abdurrazak Sulaiman <abdulhakim.a.sulaiman50@gmail.com>
2024-07-06 11:28:14 +0100

    python_net

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# This script fetches the size of the body of the response from a URL in bytes
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Sends a GET request to the URL and displays the response body for a 200 status
+curl -s -o /dev/null -w "%{http_code}" "$1" | xargs -I {} sh -c '[[ "{}" == "200" ]] && curl -s "$1"' -- "$1"
