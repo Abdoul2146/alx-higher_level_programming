0fd1073234eb1509d2b7c4ab3d270a274c97d864
Abdulhakim Abdurrazak Sulaiman <abdulhakim.a.sulaiman50@gmail.com>
2024-07-06 11:38:02 +0100

    Python_net2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL and displays the response body for a 200 status
-curl -s -o /dev/null -w "%{http_code}" "$1" | xargs -I {} sh -c '[[ "{}" == "200" ]] && curl -s "$1"' -- "$1"
+# show body
+curl -Ls $1
